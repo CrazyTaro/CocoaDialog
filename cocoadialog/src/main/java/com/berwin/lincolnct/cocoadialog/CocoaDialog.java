c6c93d3812771007606963742fc17dc9b1c17633
lincolnxu <lincolnxuhaolin@qq.com>
2022-02-14 17:38:16 +0800

    cocoadialog-修复dialog返回的消息内容控件错误的问题

--- cocoadialog/src/main/java/com/berwin/lincolnct/cocoadialog/CocoaDialog.java
+++ cocoadialog/src/main/java/com/berwin/lincolnct/cocoadialog/CocoaDialog.java
@@ -228,7 +228,7 @@ public final class CocoaDialog extends Dialog {
      */
     @Nullable
     public TextView getMessageView() {
-        return mTitleTextView;
+        return mMessageTextView;
     }
 
     /**
